8895720b41be9c215cc733f983148f2347a9d7c7
bakr27072008 <essamabobakr900@gmail.com>
2026-06-14 09:34:33 +0300

    I finsiehd the header section, only the button that reveal the menu is remaining.

---
+++ main.js
@@ -0,0 +1,26 @@
+/*============ SHOW MENU ============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/* Menu show */
+if(navToggle){
+    navToggle.addEventListener
+}
+
+/*============ REMOVE MENU MOBILE ==========*/
+
+
+/*=========== SHADOW HEADER =========*/
+
+
+/*============== SWIPER FAVORITES =========*/
+
+
+/*=========== SHOW SCROLL UP =========*/
+
+
+/*============= SCROLL SECTIONS ACTIVE LINK =========*/
+
+
+/*=============== SCROLL REVEAL ANIMATION =============*/
