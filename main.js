da588d86b485e780c70a980439d81927451de4a2
bakr27072008 <essamabobakr900@gmail.com>
2026-06-14 10:24:50 +0300

    I finished the header section, added most of the parts of the home section.

--- main.js
+++ main.js
@@ -5,11 +5,27 @@ const navMenu = document.getElementById('nav-menu'),
 
 /* Menu show */
 if(navToggle){
-    navToggle.addEventListener
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/* Menu hidden*/
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
 }
 
 /*============ REMOVE MENU MOBILE ==========*/
+const navLink = document.querySelectorAll('.nav__link')
 
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu close
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=========== SHADOW HEADER =========*/
 
